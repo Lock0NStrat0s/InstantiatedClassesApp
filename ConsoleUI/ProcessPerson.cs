ec747f79af2c539c4d59448ef656c21ff32d0a79
Mohammad Jabir <77463773+Lock0NStrat0s@users.noreply.github.com>
2023-05-13 00:23:18 -0600

    code complete.

---
+++ ConsoleUI/ProcessPerson.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI;
+
+public static class ProcessPerson
+{
+    public static void GreetPerson(PersonModel person)
+    {
+        Console.WriteLine($"Hello {person.LastName}");
+        person.HasBeenGreeted = true;
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,9 +1,38 @@
-﻿
+﻿using ConsoleUI;
 
-string firstName;
-string lastName;
-string emailAddress;
+//building a house from the blueprint
+//PersonModel person = new PersonModel();
 
-List<string> firstNames = new List<string>();
-List<string> lastNames = new List<string>();
-List<string> emailAddresses = new List<string>();
+//List<PersonModel> people = new List<PersonModel>();
+
+////variable holds the street address
+//PersonModel person = new PersonModel();
+
+//person.firstName = "Mohammad";
+
+//people.Add(person);
+
+List<PersonModel> people = new List<PersonModel>();
+string firstName = "";
+
+do
+{
+    Console.Write("What is your first name (e to exit): ");
+    firstName = Console.ReadLine();
+
+    Console.Write("What is your last name (e to exit): ");
+    string lastName = Console.ReadLine();
+
+    if (firstName.ToLower() != "e")
+    {
+        PersonModel person = new PersonModel();
+        person.FirstName = firstName;
+        person.LastName = lastName;
+        people.Add(person);
+    }
+} while (firstName.ToLower() != "e");
+
+foreach (PersonModel p in people)
+{
+    ProcessPerson.GreetPerson(p);
+}
