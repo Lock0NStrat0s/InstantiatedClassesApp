6e0e8f3280f13df4283a22432f76ab4cc03a610b
Mohammad Jabir <77463773+Lock0NStrat0s@users.noreply.github.com>
2023-05-12 23:05:45 -0600

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+string firstName;
+string lastName;
+string emailAddress;
+
+List<string> firstNames = new List<string>();
+List<string> lastNames = new List<string>();
+List<string> emailAddresses = new List<string>();
